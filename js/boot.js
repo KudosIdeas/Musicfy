be50ea69f3a5e6a7ceaa762dfc88d9bf957a9b13
Diego <diegolanda@msn.com>
2013-03-11 15:11:58 -0700

    Arreglando arquitectura
    Archivos Modificados:
    - index.html
    - style.css
    - boot.js
    
    Nota: limpieza de codigo y de arquitectura

--- js/boot.js
+++ js/boot.js
@@ -67,19 +67,9 @@ $(document).ready(function(){
 						$(this).jPlayer("setMedia", {
 							mp3: "http://api.soundcloud.com/tracks/"+mediaId+"/stream?client_id="+apiKey,
 							oga: "http://api.soundcloud.com/tracks/"+mediaId+"/download?client_id="+apiKey,
-
-							//mp3: data.stream_url + client_id,
-							//oga: data.download_url + client_id
 						});
 						
 						if(event.jPlayer.html.used && event.jPlayer.html.audio.available) {
-							/* Colors from Squares:
-							* Teal:         #6cdbd5
-							* Light Teal:   #beecea
-							* Orange:       #fd7426
-							* Light Orange: #fd9d6d
-							* Grey:         #88898b
-							*/
 						       myPlayer.jPlayer("play");
 						}
 					},
@@ -88,8 +78,8 @@ $(document).ready(function(){
 					supplied: "mp3,oga",
 					cssSelectorAncestor: "",
 					cssSelector: {
-						play: "#simple-controls .jp-play",
-						pause: "#simple-controls .jp-pause",
+						play: ".jp-play",
+						pause: ".jp-pause",
 						seekBar: "#waveform .jp-seek-bar",
 						playBar: "#waveform .jp-play-bar"
 					},
@@ -102,24 +92,6 @@ $(document).ready(function(){
 		
 		
 		SCsearch('Good Time For Psy Saxobeat Style');
-		/*
-		SC.initialize({
-                  client_id: '297aeedf702a22bba252d2b6a091960d'
-                });
-		// find all sounds of buskers licensed under 'creative commons share alike'
-		SC.get('/tracks', { q: 'Good Time For Psy Saxobeat Style' }, function(tracks) {
-			console.log(tracks);
-		
-			var obj = tracks;
-			
-			$.each(obj, function(i, item) {
-				$('#SoundCloudSearch').html( $('#SoundCloudSearch').html() + "<div class='music' onclick='SoundCloudReload("+obj[i].id+");' style:'cursor:pointer;'><img src='"+ obj[i].artwork_url +"' /><b>Nombre:</b>" + obj[i].title + "</div>");
-				$(".track-"+obj[i].id).click({id: obj[i].id},SoundCloudReload);
-				console.log(obj[i].id+" Hola<br>");
-				//alert(data[i].PageName);
-			});
-
-		});*/
 	   
 	} 
 
@@ -152,16 +124,16 @@ $(document).ready(function(){
                 });
 		// find all sounds of buskers licensed under 'creative commons share alike'
 		SC.get('/tracks', { q: question }, function(tracks) {
+			
 			console.log(tracks);
 		      
 		      var obj = tracks;
 		      
-		      $('#SoundCloudSearch').html("");
+		      $('#SCresultados').html("");
 		      
 		      $.each(obj, function(i, item) {
-			      $('#SoundCloudSearch').html( $('#SoundCloudSearch').html() + "<div class='music' onclick='SoundCloudReload("+obj[i].id+");' style:'cursor:pointer;'><img src='"+ obj[i].artwork_url +"' /><b>Nombre:</b>" + obj[i].title + "Descarga: <a href='" + obj[i].permalink_url +"' target='_blank'>Descarga</a></div>");
+			      $('#SCresultados').html( $('#SCresultados').html() + "<div class='music' onclick='SoundCloudReload("+obj[i].id+");' style:'cursor:pointer;'><img src='"+ obj[i].artwork_url +"' /><b>Nombre:</b>" + obj[i].title + "Descarga: <a href='" + obj[i].permalink_url +"' target='_blank'>Descarga</a></div>");
 			      $(".track-"+obj[i].id).click({id: obj[i].id},SoundCloudReload);
-			      console.log(obj[i].id+" Hola<br>");
 			      //alert(data[i].PageName);
 		      });
 		});
@@ -187,23 +159,6 @@ $(document).ready(function(){
 			url: "http://api.soundcloud.com/tracks/"+mediaId+".json" + client_id,
 			dataType: 'jsonp',
 			success: function(data) {
-				
-				//$(this).jPlayer("stop");
-				//
-				//$(this).jPlayer("clearMedia");
-/*
-				$(this).jPlayer("setMedia", {
-					
-					mp3: "http://api.soundcloud.com/tracks/"+mediaId+"/stream?client_id="+apiKey,
-					oga: "http://api.soundcloud.com/tracks/"+mediaId+"/download?client_id="+apiKey,
-
-							//mp3: data.stream_url + client_id,
-							//oga: data.download_url + client_id
-				});
-				
-				myPlayer.jPlayer("play");
-				
-				$("#waveform-img").attr("src", data.waveform_url);*/
 
 				myPlayer.jPlayer({
 					ready: function (event) {
@@ -216,13 +171,6 @@ $(document).ready(function(){
 						});
 						
 						if(event.jPlayer.html.used && event.jPlayer.html.audio.available) {
-							/* Colors from Squares:
-							* Teal:         #6cdbd5
-							* Light Teal:   #beecea
-							* Orange:       #fd7426
-							* Light Orange: #fd9d6d
-							* Grey:         #88898b
-							*/
 						       myPlayer.jPlayer("play");
 						}
 					},
@@ -231,8 +179,8 @@ $(document).ready(function(){
 					supplied: "mp3,oga",
 					cssSelectorAncestor: "",
 					cssSelector: {
-						play: "#simple-controls .jp-play",
-						pause: "#simple-controls .jp-pause",
+						play: ".jp-play",
+						pause: ".jp-pause",
 						seekBar: "#waveform .jp-seek-bar",
 						playBar: "#waveform .jp-play-bar"
 					},
@@ -244,8 +192,6 @@ $(document).ready(function(){
 					$("#waveform-img").attr("src", data.waveform_url);
 				}
 		});
-		
-		console.log("ENtra");
 		
 		return false;
 	}
